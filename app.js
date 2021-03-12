50e15c6ae841cac8d50e31268a033900dfaa74a9
luciaagarcia <lucia.alfonso@alu.uclm.es>
2021-03-12 20:27:59 +0100

    cosas basicas

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true)
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var postRouter = require('./routes/post');
+
+var bodyParser = require("body-parser");
+var cors = require('cors');
 
 var app = express();
+
+require('dotenv').config();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,22 +29,30 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/post', postRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
+
+app.use(cors());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extende: true }))
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
+    .then(() => console.log('mymerndb connection successful'))
+    .catch((err) => console.error(err));
+
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
